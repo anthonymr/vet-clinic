5140601f4c62857c94ec21ccffec28693ddaacb3
Anthony Martin <anthonymartin20@gmail.com>
2023-03-29 09:28:54 +0100

    Create owners and species tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,25 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
     ADD COLUMN species VARCHAR(250);
+
+CREATE TABLE owners (
+    id                  INT GENERATED ALWAYS AS IDENTITY,
+    full_name           VARCHAR(250),
+    age     SMALLINT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id                  INT GENERATED ALWAYS AS IDENTITY,
+    name                VARCHAR(250),
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+    DROP COLUMN species,
+    ADD COLUMN species_id INTEGER,
+    ADD COLUMN owners_id INTEGER,
+    ADD CONSTRAINT fk_species FOREIGN KEY (species_id)
+        REFERENCES species(id),
+    ADD CONSTRAINT fk_owners FOREIGN KEY (owners_id)
+        REFERENCES owners(id);
