1897955486a1a895e8c82a13bc8968dc0febe01c
Anthony Martin <anthonymartin20@gmail.com>
2023-03-29 09:29:05 +0100

    Insert some news species and owners

--- data.sql
+++ data.sql
@@ -43,3 +43,47 @@ INSERT INTO animals
 INSERT INTO animals 
   (name, date_of_birth, escape_attempts, neutered, weight_kg) 
   VALUES ('Ditto', '2022-05-14', 4, true, 22);
+
+INSERT INTO owners 
+  (full_name, age) 
+  VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+INSERT INTO species
+  (name)
+  VALUES
+    ('Pokemon'),
+    ('Digimon');
+
+UPDATE animals
+  SET species_id = 2
+  WHERE name like '%mon';
+
+UPDATE animals
+  SET species_id = 1
+  WHERE species_id IS NULL;
+
+UPDATE animals
+  SET owners_id = 1
+  WHERE name = 'Agumon';
+
+UPDATE animals
+  SET owners_id = 2
+  WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+  SET owners_id = 3
+  WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+  SET owners_id = 4
+  WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+  SET owners_id = 5
+  WHERE name IN ('Angemon', 'Boarmon');
